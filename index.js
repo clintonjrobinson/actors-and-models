bfaf312e3078f3a7ac5bb6d5b7428c9ad8aa7561
Clinton Robinson <clint@usful.co>
2015-02-17 18:33:44 -0500

    Updates

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 "use strict";
 
-var ObjectID = require('mongodb').ObjectID;
-var co = require('co');
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
+var ObjectID = require('mongodb').ObjectID;
+var babel = require('babel');
+var fs = require('fs');
+var uglify = require('uglify-js');
+
+var Common = require('./lib/Common');
 var Document = require('./lib/Document');
 var Structure = require('./lib/Structure');
+var Property = require('./lib/Property');
+var Validators = require('./lib/validators').Validators;
+
 var Types = require('./lib/Types');
 var utils = require('./lib/utils');
 
 var userDefinition = require('./models/User').User;
-var mongo;
 
 exports = module.exports = Models;
 
@@ -18,91 +25,14 @@ function Models(model) {
 }
 
 //TODO: Probably a better way to do this.
-//Probably model should be a part of our Mongo lib, refactor this later.
-Models.setMongoConnection = function(_mongo) {
-  mongo = _mongo;
+Models.setMongoConnection = function(mongo) {
+  Models.mongo = mongo;
   Document.setMongoConnection(mongo);
 
-  //Make sure the appropriate users have been set.
-  co(function *() {
-    yield ensureSystemUser;
-    yield ensureAnonymousUser;
-    yield ensureAdministratorUser;
-  }).catch(function (e) {
-    console.error(e);
-    console.error(e.data);
-    console.error(e.stack);
-  });
+  //Ensure that all the required users have been created.
+  require('./lib/requiredUsers')(Models);
 };
 
-function *ensureSystemUser() {
-  var User = Document.models.User;
-
-  Models.systemUser = yield mongo.findOne(User.collectionName, {login:'system'});
-
-  //No System user found.  Create one.
-  if (!Models.systemUser) {
-    let user = {
-      login: 'system',
-      name: 'System',
-      password: utils.guid(32),
-      roles: ['System']
-    };
-
-    console.log('- Actors & Models - no system user found.  Creating one.');
-    let systemUser = yield mongo.insert(User.collectionName, user);
-    Models.systemUser = new User(systemUser);
-  }
-
-  global.systemContext = {
-    session: {user: Models.systemUser},
-    user: Models.systemUser,
-    userId: Models.systemUser._id,
-    roles: ['System'],
-    isSystemContext: true
-  };
-}
-
-function *ensureAnonymousUser() {
-  var User = Document.models['User'];
-
-  try {
-    Models.anonymousUser = yield User.get(global.systemContext, {login:'anonymous'});
-  } catch (e) {
-    console.log('- Actors & Models - no anonymous user found.  Creating one.');
-    Models.anonymousUser = yield User.create(
-      global.systemContext,
-      new User({
-        login: 'anonymous',
-        name: 'Anonymous',
-        password: utils.guid(32),
-        roles: ['Anonymous']
-      }),
-      {overrideValidation: true}
-    );
-  }
-
-  global.anonymousContext = {session:{user:Models.anonymousUser}};
-}
-
-function *ensureAdministratorUser() {
-  var User = Document.models['User'];
-
-  try {
-    let administrator = yield User.get(global.systemContext, {login:'admin'});
-  } catch (e) {
-    let password = utils.guid(32);
-
-    console.log('- Actors & Models - no administrator user found.  Creating one with password: ' + password);
-    yield User.create(global.systemContext, {
-      login: 'admin',
-      name: 'System Administrator',
-      password: password,
-      roles: ['Admin']
-    });
-  }
-
-}
 
 //TODO: Implement.  Allow new validation functions to be registered
 Models.registerValidator = function() {
@@ -124,15 +54,26 @@ Models.api = function() {
 
     if (/\/api\/v1\//.test(this.path)) {
       var call = this.path.replace('/api/v1/', '').split('/');
+
+      switch (call[0]) {
+        case 'models.js':
+          this.body = Models.clientJS();
+          break;
+      }
+      if (call[0] === 'models.js') {
+      }
+
       var model = Document.models[call[0]];
+      var query = (this.request.body && this.request.body.query) || {};
+      var options = (this.request.body && this.request.body.options) || {};
 
       switch (call[1]) {
         case 'count':
-          this.body = yield model.count(this, this.request.body);
+          this.body = yield model.count(this, query, options);
           break;
 
         case 'find':
-          this.body = yield model.find(this, this.request.body);
+          this.body = yield model.find(this, query, options);
           break;
 
         case 'create':
@@ -180,6 +121,67 @@ Models.api = function() {
   }
 };
 
+var clientJS;
+Models.clientJS = function() {
+  if (!clientJS) {
+    var str = 'var ObjectID = String;\n'
+    //TODO: refactor this later.  Could be a separate client side Types library
+    str += `var utils = {
+      union: ${utils.union},
+      guid: ${utils.guid},
+      RESERVED_WORDS: ${JSON.stringify(utils.RESERVED_WORDS)},
+      RESERVED_ROLES: ${JSON.stringify(utils.RESERVED_ROLES)}
+    };\n`;
+
+    str += 'var Validators = {';
+    for (var validator in Validators) {
+      str += `${validator}: ${Validators[validator]},`;
+    }
+    str += '};\n';
+
+    str += `var Types = {
+      MiniGuid: ${Types.MiniGuid},
+      Guid: ${Types.Guid}
+    };\n`;
+
+    str += 'var Models = window.Models = {};\n';
+    str += Models.Property.toString();
+    str += `Models.Property = Property;\n`;
+    str += Models.Common.toString();
+    str += `Models.Common = Common;\n`;
+    str += Models.Structure.toString();
+    str += `Models.Structure = Structure;\n`;
+    str += `Models.structures = {};\n`;
+    str += 'var Extend = Structure;';
+
+    for (let structure in Models.structures) {
+      str += Models.structures[structure].toString();
+      str += `Models.structures.${structure} = ${structure};\n`;
+      str += `Models.structures.${structure}._clientInit();`;
+    }
+
+    str += Models.Document.toString();
+    str += `Models.Document = Document;\n`;
+    str += `Models.models = {};\n`;
+    str += 'Extend = Document;';
+    for (let model in Models.models) {
+      str += Models.models[model].toString();
+      str += `Models.models.${model} = ${model};\n`;
+      str += `Models.models.${model}._clientInit();`;
+    }
+
+    clientJS = babel.transform(str).code;
+
+    if (IS_PRODUCTION) {
+      clientJS = uglify.minify(clientJS, {fromString: true}).code;
+    }
+  }
+
+  return '(function() {\n' + clientJS + '\n})();';
+};
+
+Models.Property = Property;
+Models.Common = Common;
 Models.Document = Document;
 Models.Structure = Structure;
 Models.Types = Types;
